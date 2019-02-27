a9240b6b8f36ffb1a371c372cec064c5c7ebc5bf
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-02-26 23:23:59 -0300

    refat code - add alias to userstruct

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -1,24 +1,28 @@
 defmodule FinancialSystem do
   @moduledoc false  # TODO
   
+  alias FinancialSystem.UserStruct, as: UserStruct
+
   def create_user(name, email, currency, initial_value) do
-    %FinancialSystem.UserStruct{name: name, email: email, currency: String.upcase(currency), value: initial_value}
+    %UserStruct{name: name, email: email, currency: String.upcase(currency), value: initial_value}
   end
 
-  def deposit(%FinancialSystem.UserStruct{value: value} = account, deposit_amount) when deposit_amount > 0 do
+  def deposit(%UserStruct{value: value} = account, deposit_amount) when deposit_amount > 0 do
     new_value = value + deposit_amount
     
-    %FinancialSystem.UserStruct{account | value: new_value}
+    %UserStruct{account | value: new_value}
   end
 
-  def transfer(%FinancialSystem.UserStruct{value: value_from} = account_from,
-    %FinancialSystem.UserStruct{value: value_to} = account_to, transfer_amount) 
+  def transfer(%UserStruct{value: value_from} = account_from,
+    %UserStruct{value: value_to} = account_to, transfer_amount) 
     when value_from >= transfer_amount and transfer_amount > 0 do
     
     new_value_from = value_from - transfer_amount 
     new_value_to = transfer_amount + value_to
 
-    %FinancialSystem.UserStruct{account_to | value: new_value_to}
-    %FinancialSystem.UserStruct{account_from | value: new_value_from}
+    %UserStruct{account_to | value: new_value_to}
+    %UserStruct{account_from | value: new_value_from}
   end
+
+  def split()
 end
