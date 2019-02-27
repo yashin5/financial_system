1a6655d43de3e18c6cff3b991429b8811646dadf
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-02-26 23:15:54 -0300

    feat transfer

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -10,4 +10,15 @@ defmodule FinancialSystem do
     
     %FinancialSystem.UserStruct{account | value: new_value}
   end
+
+  def transfer(%FinancialSystem.UserStruct{value: value_from} = account_from,
+    %FinancialSystem.UserStruct{value: value_to} = account_to, transfer_amount) 
+    when value_from >= transfer_amount and transfer_amount > 0 do
+    
+    new_value_from = value_from - transfer_amount 
+    new_value_to = transfer_amount + value_to
+
+    %FinancialSystem.UserStruct{account_to | value: new_value_to}
+    %FinancialSystem.UserStruct{account_from | value: new_value_from}
+  end
 end
