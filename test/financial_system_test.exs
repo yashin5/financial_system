e045083b3c8523c4bb1cee47a24f8cec6b159120
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-03 17:45:23 -0300

    add transfer test

--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -8,15 +8,28 @@ defmodule FinancialSystemTest do
     
     assert  show_value == account1.value
   end
-  
+
   test "make deposit" do
     account1 = %FinancialSystem.Account{name: "This", email: "is", currency: "usd", value: FinancialSystem.FinHelpers.to_decimal(100)}
+
     deposit = FinancialSystem.deposit(account1, "USD", 10)
     new_value = Decimal.add(account1.value, 10) |> Decimal.round(2)
+
     account1 = %FinancialSystem.Account{name: "This", email: "is", currency: "usd", value: new_value}
 
     assert deposit.value == account1.value
   end
 
+  test "make normal transfer" do
+    account1 = %FinancialSystem.Account{name: "This", email: "is", currency: "usd", value: FinancialSystem.FinHelpers.to_decimal(100)}
+    account2 = %FinancialSystem.Account{name: "Is", email: "this", currency: "usd", value: FinancialSystem.FinHelpers.to_decimal(100)}
+
+    new_value = Decimal.add(account2.value, 50) |> Decimal.round(2)
+    make_transfer = FinancialSystem.transfer(account1, account2, 50)
+
+    account2 = %FinancialSystem.Account{name: "Is", email: "this", currency: "usd", value: new_value}
+
+    assert make_transfer.value == account2.value
+  end
 
 end
