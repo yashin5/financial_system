7066ca63f317c8752b64b622d16f2f5d9c9f633a
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-03 17:32:36 -0300

    add test get value

--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -2,6 +2,13 @@ defmodule FinancialSystemTest do
   use ExUnit.Case
   doctest FinancialSystem
 
+  test "get value from account" do
+    account1 = %FinancialSystem.Account{name: "This", email: "is", currency: "usd", value: FinancialSystem.FinHelpers.to_decimal(100)}
+    show_value = FinancialSystem.get_value_in_account(account1)
+    
+    assert  show_value == account1.value
+  end
+  
   test "make deposit" do
     account1 = %FinancialSystem.Account{name: "This", email: "is", currency: "usd", value: FinancialSystem.FinHelpers.to_decimal(100)}
     deposit = FinancialSystem.deposit(account1, "USD", 10)
@@ -10,4 +17,6 @@ defmodule FinancialSystemTest do
 
     assert deposit.value == account1.value
   end
+
+
 end
