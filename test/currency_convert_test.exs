f4c2977f77ad679160844001d892a1c43348daee
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-04 13:03:34 -0300

    add test to convert

--- test/currency_convert_test.exs
+++ test/currency_convert_test.exs
@@ -4,6 +4,20 @@ defmodule CurrencyConvert do
 
   test "get the currency rate from json file" do
     currency_rate = Map.to_list(FinancialSystem.CurrencyConvert.currency_rate()["quotes"])
+
     assert :erlang.length(currency_rate) == 168
+  end
+
+  test "perform value conversion" do
+    brl_value =
+      FinancialSystem.CurrencyConvert.currency_rate()["quotes"]["USDBRL"]
+      |> Decimal.from_float()
+      |> Decimal.round(2)
+
+    decimal_value =
+      Decimal.add(1, 0)
+      |> FinancialSystem.CurrencyConvert.convert("USD", "BRL")
+
+    assert decimal_value == brl_value
   end
 end
