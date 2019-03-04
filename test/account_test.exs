52b2f776d12b166ee95f420f31ec6e5dbd9d86a2
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-04 15:15:19 -0300

    add test to split list, create account and account struct

--- test/account_test.exs
+++ test/account_test.exs
@@ -1,1 +1,13 @@
+defmodule AccountTest do
+  use ExUnit.Case
+  doctest FinancialSystem.Account
 
+  test "create a struct for user account" do
+    assert %FinancialSystem.Account{
+      name: "This",
+      email: "is",
+      currency: "usd",
+      value: FinancialSystem.FinHelpers.to_decimal(100)
+    }
+  end
+end
--- test/create_account_test.exs
+++ test/create_account_test.exs
@@ -1,1 +1,8 @@
+defmodule CreateAccount do
+  use ExUnit.Case
+  doctest FinancialSystem.CreateAccount
 
+  test "create user" do
+    assert FinancialSystem.CreateAccount.create_user("This", "is", "brl", 100)
+  end
+end
