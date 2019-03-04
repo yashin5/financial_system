caa8aae8c34c73cc1f8ff6c6c378befa7ccecb36
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-04 12:15:12 -0300

    add test to subtract and add value

--- test/fin_helpers_test.exs
+++ test/fin_helpers_test.exs
@@ -1,1 +1,18 @@
+defmodule FinHelpersTest do
+  use ExUnit.Case
+  doctest FinancialSystem.FinHelpers
+  
+  test "make a withdrawal from account" do
+    account1 = FinancialSystem.CreateAccount.create_user("This", "is", "BRL", 100)
+    subtract_value = FinancialSystem.FinHelpers.subtracts_value(account1, account1.value, 50)
 
+    assert subtract_value.value < account1.value
+  end
+
+  test "make value add to account" do
+    account1 = FinancialSystem.CreateAccount.create_user("This", "is", "BRL", 100)
+    add_value = FinancialSystem.FinHelpers.add_value(account1, account1.value, "USD", account1.currency,  50)
+
+    assert add_value.value > account1.value
+  end
+end
