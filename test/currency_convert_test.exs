25200d458b43e453ca518fb8c101fc64573dd3ee
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-04 12:51:12 -0300

    add test to currency_rate

--- test/currency_convert_test.exs
+++ test/currency_convert_test.exs
@@ -1,1 +1,9 @@
+defmodule CurrencyConvert do
+  use ExUnit.Case
+  doctest FinancialSystem.CurrencyConvert
 
+  test "get the currency rate from json file" do
+    currency_rate = Map.to_list(FinancialSystem.CurrencyConvert.currency_rate()["quotes"])
+    assert :erlang.length(currency_rate) == 168
+  end
+end
--- test/fin_helpers_test.exs
+++ test/fin_helpers_test.exs
@@ -1,7 +1,7 @@
 defmodule FinHelpersTest do
   use ExUnit.Case
   doctest FinancialSystem.FinHelpers
-  
+
   test "make a withdrawal from account" do
     account1 = FinancialSystem.CreateAccount.create_user("This", "is", "BRL", 100)
     subtract_value = FinancialSystem.FinHelpers.subtracts_value(account1, account1.value, 50)
@@ -11,7 +11,9 @@ defmodule FinHelpersTest do
 
   test "make value add to account" do
     account1 = FinancialSystem.CreateAccount.create_user("This", "is", "BRL", 100)
-    add_value = FinancialSystem.FinHelpers.add_value(account1, account1.value, "USD", account1.currency,  50)
+
+    add_value =
+      FinancialSystem.FinHelpers.add_value(account1, account1.value, "USD", account1.currency, 50)
 
     assert add_value.value > account1.value
   end
@@ -19,7 +21,7 @@ defmodule FinHelpersTest do
   test "transform number integer or float to decimal" do
     to_decimal_integer = FinancialSystem.FinHelpers.to_decimal(10)
     to_decimal_float = FinancialSystem.FinHelpers.to_decimal(10.0)
-    decimal_test = Decimal.add(10,0) |> Decimal.round(1)
+    decimal_test = Decimal.add(10, 0) |> Decimal.round(1)
 
     assert to_decimal_integer == decimal_test and to_decimal_float == decimal_test
   end
