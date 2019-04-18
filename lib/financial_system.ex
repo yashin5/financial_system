53e468420dec6efa2b1c7fa4b7e53f4d011faa95
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-04-17 22:59:55 -0300

    create a pattern matching guard for Create function

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -1,6 +1,26 @@
 defmodule FinancialSystem do
-  def create() do
+
+  alias FinancialSystem.AccountDefinition, as: AccountDefinition
+
+  def create(%AccountDefinition{
+    name: name, currency: currency, value: value} = state)
+     when is_binary(name) and is_binary(currency) and is_number(value) do
+
   end
+
+  def create(%AccountDefinition{name: name}) when not is_binary(name) do
+    IO.inspect("Name - #{name} - must be a string.")
+  end
+
+  def create(%AccountDefinition{currency: currency}) when not is_binary(currency) do
+    IO.inspect("Currency - #{currency} - must be a string.")
+  end
+
+  def create(%AccountDefinition{value: value}) when not is_number(value) do
+    IO.inspect("Value - #{value} - must be a number.")
+  end
+
+  def create(_state), do: "Please use the correct data struct."
 
   def show() do
   end
