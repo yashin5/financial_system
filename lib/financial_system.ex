5b6c4e3bb7ade48ff5c84d7735bbac3bcf2cf86b
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-04-18 23:32:17 -0300

    feat create account

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -1,28 +1,23 @@
 defmodule FinancialSystem do
-
+  use GenServer
   alias FinancialSystem.AccountDefinition, as: AccountDefinition
+  alias FinancialSystem.AccountState, as: AccountState
 
   def create(%AccountDefinition{
-    name: name, currency: currency, value: value} = state)
+    name: name, currency: currency, value: value} = account)
      when is_binary(name) and is_binary(currency) and is_number(value) do
-
+      account
+      |> AccountState.start()
   end
 
-  def create(%AccountDefinition{name: name}) when not is_binary(name) do
-    IO.inspect("Name - #{name} - must be a string.")
-  end
-
-  def create(%AccountDefinition{currency: currency}) when not is_binary(currency) do
-    IO.inspect("Currency - #{currency} - must be a string.")
-  end
-
-  def create(%AccountDefinition{value: value}) when not is_number(value) do
-    IO.inspect("Value - #{value} - must be a number.")
+  def create(%AccountDefinition{name: name, currency: currency, value: value}) when not is_binary(name) or not is_binary(currency) or not is_number(value) do
+    IO.inspect("Name - #{name} and Currency - #{currency} must be a string and Value - #{value} must be a number.")
   end
 
   def create(_state), do: "Please use the correct data struct."
 
-  def show() do
+  def show(pid) do
+    Gen
   end
 
   def deposit() do
