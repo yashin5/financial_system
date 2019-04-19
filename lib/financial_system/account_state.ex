0228d748175d0930a60b0067b946113787548ef2
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-04-19 00:04:37 -0300

    core account state

---
+++ lib/financial_system/account_state.ex
@@ -0,0 +1,23 @@
+defmodule FinancialSystem.AccountState do
+  use GenServer
+  alias FinancialSystem.AccountDefinition, as: AccountDefinition
+
+  def start(state) do
+    GenServer.start(__MODULE__, state)
+  end
+  def init(state) do
+    {:ok, state}
+  end
+
+  def handle_call(:get_data, _, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:deposit, value}, account) do
+    {:noreply, %AccountDefinition{account | value: account.value + value}}
+  end
+
+  def handle_cast({:withdraw, value}, account) do
+    {:noreply, %AccountDefinition{account | value: account.value - value}}
+  end
+end
