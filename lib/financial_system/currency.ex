f3c285588f1d2aa82268a7c9a0dc07e7e527da64
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-04-21 11:17:38 -0300

    add @spec

--- lib/financial_system/currency.ex
+++ lib/financial_system/currency.ex
@@ -1,18 +1,19 @@
 defmodule FinancialSystem.Currency do
-  def currency_rate do
+  @spec currency_rate() :: map()
+  defp currency_rate do
     case File.read("currency_rate.json") do
       {:ok, body} -> Poison.decode!(body)
       {:error, reason} -> reason
     end
   end
 
+  @spec currency_is_valid?(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def currency_is_valid?(currency) do
     is_valid? = Map.has_key?(currency_rate()["quotes"], "USD#{String.upcase(currency)}")
 
     case is_valid? do
       true ->
         {:ok, String.upcase(currency)}
-
       false ->
         {:error,
          raise(ArgumentError,
@@ -21,13 +22,12 @@ defmodule FinancialSystem.Currency do
     end
   end
 
+  @spec convert(String.t(), pid(), number()) :: number()
   def convert("USD", currency_to, value) do
-    mult = &(&1 * currency_rate()["quotes"]["USD#{String.upcase(currency_to)}"])
-
-    value
-    |> mult.()
+    value * currency_rate()["quotes"]["USD#{String.upcase(currency_to)}"]
   end
 
+  @spec convert(String.t(), String.t(), number()) :: number()
   def convert(currency_from, currency_to, value) do
     value / currency_rate()["quotes"]["USD#{String.upcase(currency_from)}"] *
       currency_rate()["quotes"]["USD#{String.upcase(currency_to)}"]
