d5df5be610b9d8a8a8d6074ea9bee3a0c0a832f9
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-05-01 16:22:35 -0300

    refat

--- lib/financial_system/financial_system_financial.ex
+++ lib/financial_system/financial_system_financial.ex
@@ -2,14 +2,14 @@ defmodule FinancialSystem.Financial do
   @moduledoc false
   alias FinancialSystem.{Account, Split}
 
-  @callback create(String.t() | any(), String.t() | any(), number() | any()) ::
+  @callback create(String.t() | any(), String.t() | any(), String.t() | any()) ::
               {:ok, pid()} | {:error, no_return()} | no_return()
-  @callback show(pid() | any()) :: Decimal.t() | no_return()
-  @callback deposit(pid() | any(), String.t() | any(), number() | any()) ::
+  @callback show(pid() | any()) :: String.t() | no_return()
+  @callback deposit(pid() | any(), String.t() | any(), String.t() | any()) ::
               Account.t() | no_return()
-  @callback withdraw(pid() | any(), number() | any()) ::
+  @callback withdraw(pid() | any(), String.t() | any()) ::
               Account.t() | {:error, no_return()} | no_return()
-  @callback transfer(number(), pid(), pid()) :: Split.t() | no_return()
-  @callback split(pid() | any(), Split.t() | any(), number() | any()) ::
+  @callback transfer(String.t(), pid(), pid()) :: Split.t() | no_return()
+  @callback split(pid() | any(), Split.t() | any(), String.t() | any()) ::
               [Account.t()] | {:error, no_return()} | no_return()
 end
