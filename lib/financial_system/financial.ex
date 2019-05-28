5da21aca28da9f66b3a31b483de83379a00b76a3
Yashin Santos <yashinndsantos@gmail.com>
2019-05-28 15:20:20 -0300

    fix @spec

--- lib/financial_system/financial.ex
+++ lib/financial_system/financial.ex
@@ -6,9 +6,11 @@ defmodule FinancialSystem.Financial do
   @callback deposit(String.t() | any(), String.t() | any(), String.t() | any()) ::
               {:ok, Account.t()} | {:error, String.t()}
   @callback withdraw(String.t() | any(), String.t() | any()) ::
-              {:ok, Account.t()} | {:error, String.t()}
+              {:ok, Account.t()}
+              | {:error, :invalid_account_id_type | :invalid_arguments_type | :invalid_value_type}
   @callback transfer(String.t() | any(), String.t() | any(), String.t() | any()) ::
-              {:ok, Account.t()} | {:error, String.t()}
+              {:ok, Account.t()}
+              | {:error, :invalid_account_id_type | :invalid_arguments_type | :invalid_value_type}
   @callback split(String.t() | any(), list(Split.t()) | any(), String.t() | any()) ::
               {:ok, Account.t()} | {:error, String.t()}
 end
