e2a58c11e189216c157a067b8c993fe6eaa8201d
Yashin Santos <yashinndsantos@gmail.com>
2019-05-31 10:09:06 -0300

    fix @specs

--- lib/financial_system/financial.ex
+++ lib/financial_system/financial.ex
@@ -4,14 +4,20 @@ defmodule FinancialSystem.Financial do
 
   @callback show(String.t() | any()) :: {:ok, String.t()} | {:error, atom()}
   @callback deposit(String.t() | any(), String.t() | any(), String.t() | any()) ::
-              {:ok, Account.t()} | {:error, atom()}
+              {:ok, Account.t()}
+              | {:error, atom()}
+              | no_return()
   @callback withdraw(String.t() | any(), String.t() | any()) ::
               {:ok, Account.t()}
               | {:error, atom()}
               | atom()
+              | no_return()
   @callback transfer(String.t() | any(), String.t() | any(), String.t() | any()) ::
               {:ok, Account.t()}
               | {:error, atom()}
+              | no_return()
   @callback split(String.t() | any(), list(Split.t()) | any(), String.t() | any()) ::
-              {:ok, Account.t()} | {:error, atom()}
+              {:ok, Account.t()}
+              | {:error, atom()}
+              | no_return()
 end
