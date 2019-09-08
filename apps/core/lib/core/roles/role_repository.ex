9e1348e6ecca81e752695e2ad107d89532c213d7
yashin <yashinndsantos@gmail.com>
2019-09-08 14:40:41 -0300

    feat: add tests to role

--- apps/core/lib/core/roles/role_repository.ex
+++ apps/core/lib/core/roles/role_repository.ex
@@ -2,8 +2,6 @@ defmodule FinancialSystem.Core.Roles.RoleRepository do
   alias FinancialSystem.Core.Repo
   alias FinancialSystem.Core.Roles.Role
 
-  def get_role(:all), do: Role |> Repo.all()
-
   def get_role(role) do
     Role
     |> Repo.get_by(role: role)
