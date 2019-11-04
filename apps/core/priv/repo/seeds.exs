2a2ecedc743b44eaa09485056d70530bd2d6ae6b
Yashin Santos <yashinndsantos@gmail.com>
2019-11-04 19:12:33 -0300

    fix: seeds not duplicate tables

--- apps/core/priv/repo/seeds.exs
+++ apps/core/priv/repo/seeds.exs
@@ -1,45 +1,16 @@
-defmodule FinancialSystem.Core.Repo.Seeds do
   alias FinancialSystem.Core.Accounts.Account
+  alias FinancialSystem.Core.Helpers
   alias FinancialSystem.Core.Permissions.Permission
   alias FinancialSystem.Core.Repo
   alias FinancialSystem.Core.Roles.Role
   alias FinancialSystem.Core.Users.User
 
-  admin_role =
-    %Role{}
-    |> Role.changeset(%{role: "admin"})
-    |> Repo.insert!()
+  conflict_options = fn fields ->
+    [conflict_target: fields, on_conflict: :nothing]
+  end
 
-  admin_permission = %{
-    can_view: true,
-    can_create: true,
-    can_delete: true,
-    can_view_all: true
-  }
-
-  admin_role
-  |> Ecto.build_assoc(:permission, admin_permission)
-  |> Permission.changeset(admin_permission)
-  |> Repo.insert!()
-
-  regular_role =
-    %Role{}
-    |> Role.changeset(%{role: "regular"})
-    |> Repo.insert!()
-
-  regular_permission = %{
-    can_view: true,
-    can_create: true,
-    can_delete: true,
-    can_view_all: false
-  }
-
-  regular_role
-  |> Ecto.build_assoc(:permission, regular_permission)
-  |> Permission.changeset(regular_permission)
-  |> Repo.insert!()
-
-  {:ok, super_user} =
+  create_super_user = fn ->
+    {:ok, super_user} =
     %User{}
     |> User.changeset(%{
       role: "admin",
@@ -57,4 +28,45 @@ defmodule FinancialSystem.Core.Repo.Seeds do
     value: "10000"
   })
   |> Repo.insert()
+
+
+  admin_role =
+    %Role{}
+    |> Role.changeset(%{role: "admin"})
+    |> Repo.insert!(conflict_options.([:role]))
+
+  admin_permission = %{
+    can_view: true,
+    can_create: true,
+    can_delete: true,
+    can_view_all: true
+  }
+
+  admin_role
+  |> Ecto.build_assoc(:permission, admin_permission)
+  |> Permission.changeset(admin_permission)
+  |> Repo.insert!()
+
+  regular_role =
+    %Role{}
+    |> Role.changeset(%{role: "regular"})
+    |> Repo.insert!(conflict_options.([:role]))
+
+  regular_permission = %{
+    can_view: true,
+    can_create: true,
+    can_delete: true,
+    can_view_all: false
+  }
+
+  regular_role
+  |> Ecto.build_assoc(:permission, regular_permission)
+  |> Permission.changeset(regular_permission)
+  |> Repo.insert!()
 end
+
+case Helpers.get_account_or_user(:account, :email, %{"email" => "qqwqw@gmail.com"}) do
+  {:ok, _} -> :ok
+  {:error, _} -> create_super_user.()
+end
+
